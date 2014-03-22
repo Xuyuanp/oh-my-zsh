aa86f29961d495f3816280947cf54854aec6d0bd
Xuyuan Pang <xuyuanp@gmail.com>
2014-03-22 11:09:45 +0800

    path

--- install.zsh
+++ install.zsh
@@ -1,1 +1,1 @@
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git custom/plugins/zsh-syntax-highlighting/
